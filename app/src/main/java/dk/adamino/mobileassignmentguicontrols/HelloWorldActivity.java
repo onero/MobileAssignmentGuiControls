46f84218dc32546c3ca456dd9779e0febc57d1dd
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2018-02-19 12:58:58 +0100

    Added id's to widgets

--- app/src/main/java/dk/adamino/mobileassignmentguicontrols/HelloWorldActivity.java
+++ app/src/main/java/dk/adamino/mobileassignmentguicontrols/HelloWorldActivity.java
@@ -4,9 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.ToggleButton;
 
 public class HelloWorldActivity extends AppCompatActivity {
 
+    private ProgressBar mProgressBar;
+    private RadioButton mRadioButtonOne, mRadioButtonTwo, mRadioButtonThree;
+    private ToggleButton mToggleButtonOne, mToggleButtonTwo, mToggleButtonThree;
 
 
     public static Intent newIntent(Context packageContext){
@@ -18,5 +24,19 @@ public class HelloWorldActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+
+        initializeButtons();
+    }
+
+    private void initializeButtons(){
+        mProgressBar = findViewById(R.id.pbProgress);
+
+        mRadioButtonOne = findViewById(R.id.rbtnValueOne);
+        mRadioButtonTwo = findViewById(R.id.rbtnValueTwo);
+        mRadioButtonThree = findViewById(R.id.rbtnValueThree);
+
+        mToggleButtonOne = findViewById(R.id.tbtnValueOne);
+        mToggleButtonTwo = findViewById(R.id.tbtnValueTwo);
+        mToggleButtonThree = findViewById(R.id.tbtnValueThree);
     }
 }
