8911da1554dc22d16cda80031ea96e9a58f86a73
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2018-02-19 14:14:38 +0100

    Implemented Model

--- app/src/main/java/dk/adamino/mobileassignmentguicontrols/GreenActivity.java
+++ app/src/main/java/dk/adamino/mobileassignmentguicontrols/GreenActivity.java
@@ -3,15 +3,49 @@ package dk.adamino.mobileassignmentguicontrols;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 /**
  * Created by Mathias on 19/02/2018.
  */
 
 public class GreenActivity extends AppCompatActivity {
+
+    private Spinner spinner1, spinner2, spinner3;
+    private NumberPicker numberPicker1, numberPicker2, numberPicker3;
+    private Button btnSolve;
+    private TextView txtResult;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_green);
+
+        setupSpinners();
+        setupNumberPickers();
+        setupResult();
     }
+
+    private void setupResult() {
+        btnSolve = findViewById(R.id.btnSolve);
+        txtResult = findViewById(R.id.txtResult);
+    }
+
+    private void setupNumberPickers() {
+        numberPicker1 = findViewById(R.id.numberPicker1);
+        numberPicker2 = findViewById(R.id.numberPicker2);
+        numberPicker3 = findViewById(R.id.numberPicker3);
+    }
+
+    private void setupSpinners() {
+        spinner1 = findViewById(R.id.spinner1);
+        spinner2 = findViewById(R.id.spinner2);
+        spinner3 = findViewById(R.id.spinner3);
+
+    }
+
+
 }
---
+++ app/src/main/java/dk/adamino/mobileassignmentguicontrols/model/GreenModel.java
@@ -0,0 +1,19 @@
+package dk.adamino.mobileassignmentguicontrols.model;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
+/**
+ * Created by Mathias on 19/02/2018.
+ */
+
+public class GreenModel {
+
+    private final String[] weekDayList = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+            "Saturday", "Sunday"};
+    private final String[] monthList = {"January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"};
+    //private final String[] YearList;
+    //private final Integer[] numberList;
+
+}
